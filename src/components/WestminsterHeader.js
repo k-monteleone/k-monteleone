4845d3ea85fbf8f1e392af748c8334f3cc437abb
k-monteleone <151402990+k-monteleone@users.noreply.github.com>
2023-11-19 14:21:47 -0500

    fix formatting for smaller screens (#11)

--- src/components/WestminsterHeader.js
+++ src/components/WestminsterHeader.js
@@ -1,10 +1,29 @@
 import React from "react";
+import { useState } from "react";
+import { useEffect } from "react";
 import './WestminsterHeader.css';
 
 function WestmisnterHeader() {
+
+    const [isBig, setSize] = useState(false);
+
+    const showBig = () => {
+        if (window.innerWidth <= 1060) {
+            setSize(false);
+        } else {
+            setSize(true);
+        }
+    };
+
+    useEffect(() => {
+        showBig()
+    }, []);
+
+    window.addEventListener('resize', showBig)
+
     return (
         <div className="westminster-homepage">
-            <h1 className="westminster-header">Westminster College</h1>
+            <h1 className={isBig ? 'westminster-header':'westminster-header-small'}>Westminster College</h1>
         </div>
     )
 }
