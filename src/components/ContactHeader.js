3384e5e9ad840c9a49280b1b3e1ff5dca3e19147
Kyle Monteleone <monteleonekyle@gmail.com>
2023-11-19 15:46:40 -0500

    fixing cards and format issues

--- src/components/ContactHeader.js
+++ src/components/ContactHeader.js
@@ -1,10 +1,28 @@
 import React from "react";
+import { useState } from "react";
+import { useEffect } from "react";
 import './ContactHeader.css';
 
 function ContactHeader() {
+    const [isBig, setSize] = useState(false);
+
+    const showBig = () => {
+        if (window.innerWidth <= 1060) {
+            setSize(false);
+        } else {
+            setSize(true);
+        }
+    };
+
+    useEffect(() => {
+        showBig()
+    }, []);
+
+    window.addEventListener('resize', showBig)
+    
     return (
         <div className="contact-homepage">
-            <h1 className="contact-header">Contact Me</h1>
+            <h1 className={isBig ? 'contact-header':'contact-header-small'}>Contact Me</h1>
         </div>
     )
 }
