ffda6bce6636fcd360bc759b00d7b93cea382b3d
Kyle Monteleone <monteleonekyle@gmail.com>
2023-11-19 16:12:08 -0500

    another teak for thin view windows

--- src/components/ContactCardItem.js
+++ src/components/ContactCardItem.js
@@ -1,6 +1,25 @@
 import React from 'react';
+import { useState } from "react";
+import { useEffect } from "react";
 
 function CardItem(props) {
+
+  const [isBig, setSize] = useState(false);
+
+  const showBig = () => {
+      if (window.innerWidth <= 1060) {
+          setSize(false);
+      } else {
+          setSize(true);
+      }
+  };
+
+  useEffect(() => {
+      showBig()
+  }, []);
+
+  window.addEventListener('resize', showBig)
+
   return (
     <>
       <li className='contact-cards__item'>
@@ -13,7 +32,7 @@ function CardItem(props) {
             />
           </figure>
           <div className='contact-cards__item__info'>
-            <h5 className='contact-cards__item__text'>{props.text}</h5>
+            <h5 className={isBig ? 'contact-cards__item__text':'contact-cards__item__text__small'}>{props.text}</h5>
           </div>
         </p>
       </li>
